8a23d375279d74560601b698202275657d9b75e0
Zyphorah <zyphorah.zen@gmail.com>
2025-11-22 04:27:18 -0500

    Ajouter une clé étrangère manquante dans la classe CartItemDto

--- 521.tpfinal.api/models/ShoppingCart.cs
+++ 521.tpfinal.api/models/ShoppingCart.cs
@@ -1,5 +1,6 @@
 public class ShoppingCartDto
 {
+    public required int UserId { get; set; }
     public required int Id { get; set; }
     public required List<ProductDto> Products { get; set; }
     public required decimal TotalPrice { get; set; }
